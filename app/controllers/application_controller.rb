0daa8ec547a67e5761a5cbcfe9695c9c53933c37
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-07-15 13:55:55 -0400

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,23 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+
+    get '/' do
+      erb :index
+    end
+
+    post '/teams' do
+      @team = Team.new(params[:team][:name], params[:team][:motto])
+      params[:team][:member].each do |hash|
+        this_member = Member.new
+        hash.each{|key, value| this_member.send("#{key.to_sym}=", value)}
+      end
+      @members = Member.all
+      erb :team
+    end
 
 
 end
