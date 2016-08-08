dfd26c6e4308eee03ae892c0b80f1f4636eb2f42
ozPop <ozziepopovas@gmail.com>
2016-08-08 10:46:17 -0400

    update views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,13 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :team
+    end
+
+    get '/teams' do
+      # handle data to show
+      erb :super_hero.erb
+    end
 
 end
