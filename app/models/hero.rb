70156546b33c96e41728e141a001160028f221c2
Bibi Khadiza <limakhadiza@yahoo.com>
2016-08-31 15:44:31 -0400

    done

---
+++ app/models/hero.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+
+class Hero
+
+  attr_accessor :name, :power, :bio
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
