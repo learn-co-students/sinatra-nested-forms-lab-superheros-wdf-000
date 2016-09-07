9a4f2c407fe31215dad6287a31bc0456b8dab89a
Kelene Chung <simoy93@gmail.com>
2016-09-07 15:05:53 -0400

    Sinatra Nested Forms Lab Superheros DONE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
+require 'pry'
 require 'sinatra/base'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do 
+
+    	erb :super_hero
+    	
+    end
+
+    post '/teams' do 
+    	@squad = params[:team]
+    	erb :team
+    end
 
 end
